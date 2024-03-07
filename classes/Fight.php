39f67f571f44d171a1ce726fa15afa3a0b78b295
Cloud <ruben.as532@gmail.com>
2024-03-07 18:30:53 +0100

    Randomness of the hit generated

--- classes/Fight.php
+++ classes/Fight.php
@@ -1,5 +1,6 @@
 <?php
 class Fight {
+    //TO-DO: Possible refactor. Make this consts an enum?
     const LOW_AIM = 30;  
     const MED_AIM = 50;
     const HIGH_AIM = 70;
@@ -9,9 +10,13 @@ class Fight {
     public function fight() {
         while($this->areFightersAlive()) {
             //Decisión de diseño, puedo hacerlo desde fighter 1, o no.
-            $fighter1_aim = $this->getAim();
-            var_dump($fighter1_aim);
-            exit();
+            if($this->fighter1hits()) {
+                //Resto vida a fighter 2
+            }
+            else {
+                //resto vida a fighter 1
+            }
+            
         }
     }
 
@@ -19,10 +24,14 @@ class Fight {
         return $this->fighter1->getLife() > 0 && $this->fighter2->getLife() > 0;
     }
 
-    private function getAim(): int {
-        if ($this->isFighter1Stronger()) return self::HIGH_AIM;
-        else if ($this->isFighter2Stronger()) return self::LOW_AIM;
-        else return self::MED_AIM;
+    private function fighter1hits(): bool {
+        $hit_aim = self::MED_AIM;
+        $random_number = rand(1,100);
+        if ($this->isFighter1Stronger()) $hit_aim = self::HIGH_AIM;
+        else if ($this->isFighter2Stronger()) $hit_aim = self::LOW_AIM;
+
+        if($hit_aim <= $random_number) return true;
+        return false;
     }
 
     private function isFighter1Stronger(): bool {
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once('classes/Fighter.php');
 require_once('classes/Fight.php');
 //TO-DO: Input restrictions
-$fighter1 = new Fighter("Pepe",5,3);
+$fighter1 = new Fighter("Pepe",6,3);
 $fighter2 = new Fighter("Paco",5,4);
 
 $fight = new Fight($fighter1, $fighter2);
