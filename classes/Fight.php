ebce291aba46e2ce8518731ff427f07354617a1a
Cloud <ruben.as532@gmail.com>
2024-03-07 10:51:11 +0100

    Added Fight Class

---
+++ classes/Fight.php
@@ -0,0 +1,6 @@
+<?php
+class Fight {
+    function __construct(private Fighter $fighter1, private Fighter $fighter2){}
+}
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php 
 include_once('classes/Fighter.php');
+include_once('classes/Fight.php');
 //TO-DO: Input restrictions
 $fighter1 = new Fighter("Pepe",7,3);
 $fighter2 = new Fighter("Paco",6,4);
 
-var_dump($fighter1->getName());
-var_dump($fighter2->getName());
+$fight = new Fight($fighter1, $fighter2);
+
+var_dump($fight);
 
 
 ?>
